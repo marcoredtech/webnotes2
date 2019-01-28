6c61e634e7043172c212cb340bfd08c954979057
Marco Novelli <marconovelli.85@gmail.com>
2019-01-28 18:02:31 +0100

    start application

---
+++ src/main/java/com/ap/webnotes/WebnotesApplication.java
@@ -0,0 +1,14 @@
+package com.ap.webnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebnotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebnotesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/ap/webnotes/WebnotesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ap.webnotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebnotesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
